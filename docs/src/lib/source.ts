3b0dd0f0889ecab91b2e87e239c60053b965a15b
Johannes Napitupulu <johannes.napitupulu@eproc-gov.tech>
2025-10-01 00:57:10 +0700

    Fix GA

--- docs/src/lib/source.ts
+++ docs/src/lib/source.ts
@@ -1,6 +1,6 @@
 import { docs } from '@/.source';
 import { loader } from 'fumadocs-core/source';
-import { createOpenAPI, attachFile } from 'fumadocs-openapi/server';
+import { createOpenAPI } from 'fumadocs-openapi/server';
 import { icons } from 'lucide-react';
 import { createElement } from 'react';
 
@@ -13,10 +13,6 @@ export const source = loader({
       return createElement(icons[icon as keyof typeof icons]);
   },
   source: docs.toFumadocsSource(),
-  pageTree: {
-    // adds a badge to each page item in page tree
-    attachFile,
-  },
 });
 
 export const openapi = createOpenAPI({
